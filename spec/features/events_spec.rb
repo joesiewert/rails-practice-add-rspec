91d61bf5c7f6aacbb5c9b0b4e05f656780e32acd
joesiewert <joe.siewert@gmail.com>
2014-11-04 11:58:32 -0700

    Setup tests

---
+++ spec/features/events_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+feature "Events" do
+
+  scenario "User creates an event" do
+    visit root_path
+    expect(page).to have_no_content("Just a test event")
+    click_on "New Event"
+    fill_in "Description", with: "Just a test event"
+    fill_in "Location", with: "Here"
+    fill_in "Date", with: "11/15/2014"
+    fill_in "Capacity", with: 50
+    check "Requires ID"
+    click_on "Create Event"
+    expect(page).to have_content("Just a test event")
+  end
+
+  scenario "User views an event" do
+    visit root_path
+    expect(page).to have_no_content("Just a test event")
+    click_on "New Event"
+    fill_in "Description", with: "Just a test event"
+    fill_in "Location", with: "Here"
+    fill_in "Date", with: "11/15/2014"
+    fill_in "Capacity", with: 50
+    check "Requires ID"
+    click_on "Create Event"
+    expect(page).to have_content("Just a test event")
+    click_on "Just a test event"
+    expect(page).to have_content(50)
+  end
+
+  scenario "User edits an event" do
+    Event.create!(
+      description: "My aweeesome event"
+    )
+
+    visit root_path
+    expect(page).to have_content("My aweeesome event")
+    click_on "edit"
+    fill_in "Description", with: "My awesome event"
+    click_on "Update Event"
+
+    expect(page).to have_content("My awesome event")
+    expect(page).to have_no_content("My aweeesome event")
+  end
+
+  scenario "User deletes an event" do
+    Event.create!(
+      description: "Dumb event",
+      capacity: 45
+    )
+    visit root_path
+    
+  end
+
+end
